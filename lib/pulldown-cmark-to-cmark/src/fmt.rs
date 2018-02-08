fb3ba6140aca310b84e47e8266425284f8098b5f
Sebastian Thiel <byronimo@gmail.com>
2018-02-08 19:20:43 +0100

    first 'cmark' version with options

--- lib/pulldown-cmark-to-cmark/src/fmt.rs
+++ lib/pulldown-cmark-to-cmark/src/fmt.rs
@@ -1,23 +1,58 @@
 use std::fmt;
+use std::borrow::Borrow;
 use pulldown_cmark::Event;
 use display;
 
-#[derive(Default, Clone, Debug)]
-pub struct CmarkState;
+#[derive(Default, Clone, Debug, PartialEq, Eq)]
+pub struct State {
+    pub newlines_before_start: usize,
+}
 
-pub fn cmark<'a, I, E, F>(
+#[derive(Clone, Debug)]
+pub struct Options {
+    pub newlines_after_headline: usize,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Options {
+            newlines_after_headline: 2,
+        }
+    }
+}
+
+pub fn cmark_with_options<'a, I, E, F>(
     events: I,
     mut f: F,
-    state: Option<CmarkState>,
-) -> Result<CmarkState, fmt::Error>
+    state: Option<State>,
+    options: Options,
+) -> Result<State, fmt::Error>
 where
     I: Iterator<Item = E>,
-    E: ::std::borrow::Borrow<Event<'a>>,
+    E: Borrow<Event<'a>>,
     F: fmt::Write,
 {
-    let state = state.unwrap_or_default();
+    let mut state = state.unwrap_or_default();
     for event in events {
+        use pulldown_cmark::Event::*;
+        use pulldown_cmark::Tag::*;
+        match *event.borrow() {
+            End(ref tag) => match *tag {
+                Header(_) => state.newlines_before_start += options.newlines_after_headline,
+                _ => {},
+            },
+            _ => {},
+        }
         write!(f, "{}", display::Event(event.borrow()))?;
     }
     Ok(state)
+}
+
+pub fn cmark<'a, I, E, F>(events: I, f: F, state: Option<State>) -> Result<State, fmt::Error>
+where
+    I: Iterator<Item = E>,
+    E: Borrow<Event<'a>>,
+    F: fmt::Write,
+{
+    cmark_with_options(events, f, state, Options::default())
 }
--- lib/pulldown-cmark-to-cmark/tests/fmt.rs
+++ lib/pulldown-cmark-to-cmark/tests/fmt.rs
@@ -1,20 +1,29 @@
 extern crate pulldown_cmark;
 extern crate pulldown_cmark_to_cmark;
 
-use pulldown_cmark_to_cmark::fmt::cmark;
+use pulldown_cmark_to_cmark::fmt::{cmark, State};
 use pulldown_cmark::{Options, Parser};
 
-fn fmt(s: &str) -> String {
+fn fmt(s: &str) -> (String, State) {
     let mut buf = String::new();
-    cmark(Parser::new_ext(s, Options::all()), &mut buf, None).unwrap();
-    buf
+    let s = cmark(Parser::new_ext(s, Options::all()), &mut buf, None).unwrap();
+    (buf, s)
 }
 
 mod lazy_newlines {
     use super::fmt as f;
+    use super::State;
 
     #[test]
     fn after_headline() {
-        assert_eq!(f("## headline"), "## headline")
+        assert_eq!(
+            f("## headline"),
+            (
+                "## headline".into(),
+                State {
+                    newlines_before_start: 2,
+                }
+            )
+        )
     }
 }

d59def8ecdb575a5633d8f3e243e873008a28468
Sebastian Thiel <byronimo@gmail.com>
2018-02-13 09:41:26 +0100

    One step closer to actually executing a program

--- lib/termbook/src/preprocess.rs
+++ lib/termbook/src/preprocess.rs
@@ -9,43 +9,54 @@ use pulldown_cmark_to_cmark::fmt::cmark;
 pub struct RunShellScript;
 
 enum Action {
-    Exec(usize),
+    Exec {
+        program: String,
+        desired_exit_status: usize,
+    },
 }
 
 impl Action {
-    fn from_str(key: &str, val: Option<&str>) -> Option<Action> {
-        match key {
-            "exec" => Some(Action::Exec(0)),
+    fn from_str(program: &str, key: &str, val: Option<&str>) -> Result<Option<Action>, Error> {
+        Ok(match key {
+            "exec" => Some(Action::Exec {
+                program: program.to_owned(),
+                desired_exit_status: match val {
+                    Some(val) => val.parse().map_err(|e| {
+                        format!("Failed to parse integer for exec key with error: {}", e)
+                    })?,
+                    None => 0,
+                },
+            }),
             _ => None,
-        }
+        })
     }
 }
 
 #[derive(Default)]
 struct State {
-    actions: Option<Vec<Action>>,
+    actions: Vec<Action>,
     code: String,
     error: Option<Error>,
 }
 
-fn parse_actions(info: &str) -> Option<Vec<Action>> {
-    let mut res = None::<Vec<_>>;
-    for token in info.trim().split(',') {
-        let mut kvi = token.splitn(2, '=');
-        if let Some(action) = match (kvi.next().map(str::trim), kvi.next().map(str::trim)) {
-            (Some(key), possible_value) => Action::from_str(key, possible_value),
-            _ => None,
-        } {
-            res = match res {
-                Some(mut v) => {
-                    v.push(action);
-                    Some(v)
-                }
-                None => Some(vec![action]),
+fn parse_actions(info: &str) -> Result<Vec<Action>, Error> {
+    let mut res = Vec::new();
+    let mut shell = "bash";
+    for (tid, token) in info.trim().split(',').enumerate() {
+        if tid == 0 {
+            shell = token;
+        } else {
+            let mut kvi = token.splitn(2, '=');
+            let optional_action = match (kvi.next().map(str::trim), kvi.next().map(str::trim)) {
+                (Some(key), possible_value) => Action::from_str(shell, key, possible_value)?,
+                _ => None,
+            };
+            if let Some(action) = optional_action {
+                res.push(action);
             }
         }
     }
-    res
+    Ok(res)
 }
 
 fn event_filter<'a>(state: &mut &mut State, event: Event<'a>) -> Option<Vec<Event<'a>>> {
@@ -54,7 +65,30 @@ fn event_filter<'a>(state: &mut &mut State, event: Event<'a>) -> Option<Vec<Even
 
     match event {
         Start(CodeBlock(ref info)) => {
-            state.actions = parse_actions(info);
+            state.actions = match parse_actions(info) {
+                Ok(a) => a,
+                Err(e) => {
+                    state.error = Some(e);
+                    Vec::new()
+                }
+            };
+        }
+        Text(ref text) => {
+            if !state.actions.is_empty() {
+                state.code.push_str(text);
+            }
+        }
+        End(CodeBlock(_)) => {
+            for action in &state.actions {
+                match *action {
+                    Action::Exec {
+                        ref program,
+                        desired_exit_status,
+                    } => {}
+                }
+            }
+            state.actions.clear();
+            state.code.clear();
         }
         _ => {}
     };
@@ -62,9 +96,9 @@ fn event_filter<'a>(state: &mut &mut State, event: Event<'a>) -> Option<Vec<Even
 }
 
 fn process_chapter(item: &mut BookItem) -> Result<(), Error> {
+    let mut state = State::default();
     if let &mut BookItem::Chapter(ref mut chapter) = item {
-        let (md, state) = {
-            let mut state = State::default();
+        let md = {
             let mut md = String::with_capacity(chapter.content.len() + 128);
             {
                 let parser = Parser::new(&chapter.content)
@@ -72,7 +106,7 @@ fn process_chapter(item: &mut BookItem) -> Result<(), Error> {
                     .flat_map(|events| events);
                 cmark(parser, &mut md, None).map_err(|e| format!("{}", e))?;
             }
-            (md, state)
+            md
         };
         if let Some(err) = state.error {
             return Err(err);

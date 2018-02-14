bf885b074d44161eb5ca96b79877fac010438711
Sebastian Thiel <byronimo@gmail.com>
2018-02-14 20:53:45 +0100

    PoC of the DelayWriter
    It emulates a typist writing real fast.

--- lib/termbook/src/playback.rs
+++ lib/termbook/src/playback.rs
@@ -6,14 +6,39 @@ use pulldown_cmark::Parser;
 
 use mdcat::{push_tty, ResourceAccess, Terminal, TerminalSize};
 
-use std::io::stdout;
+use std::io::{self, stdout, Write};
 use std::env::current_dir;
+use std::thread::sleep;
+use std::time::Duration;
 
 pub struct Playback;
 
 impl Playback {
     pub fn new() -> Playback {
         Playback
+    }
+}
+
+struct DelayPrinter<W>
+where
+    W: Write,
+{
+    inner: W,
+}
+
+impl<W> Write for DelayPrinter<W>
+where
+    W: Write,
+{
+    fn write(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
+        sleep(Duration::from_millis(20));
+        let res = self.inner.write(buf);
+        self.inner.flush().ok();
+        res
+    }
+
+    fn flush(&mut self) -> Result<(), io::Error> {
+        self.inner.flush()
     }
 }
 
@@ -28,7 +53,7 @@ impl Renderer for Playback {
             if let &BookItem::Chapter(ref chapter) = item {
                 let syntax_set = SyntaxSet::load_defaults_newlines();
                 push_tty(
-                    &mut stdout(),
+                    &mut DelayPrinter { inner: stdout() },
                     Terminal::BasicAnsi,
                     TerminalSize {
                         width: 80,

b1ee7ea267a71f5e2af759c583e9376fb245c643
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-04-12 11:16:47 +0800

    update termbook homebrew file
    [skip CI]

--- pkg/brew/termbook.rb
+++ pkg/brew/termbook.rb
@@ -1,15 +1,15 @@
 class Termbook < Formula
   # ---> DO NOT EDIT <--- (this file was generated from ./pkg/brew/termbook.rb.in
-  version '1.4.1'
+  version 'v1.4.5'
   desc "A runner for `mdbooks` to keep your documentation tested."
   homepage "https://github.com/Byron/termbook"
 
   if OS.mac?
       url "https://github.com/Byron/termbook/releases/download/#{version}/termbook-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "0b0782098ffb96209a02bd889fcdaf1c100f131889d67bf6ff16d9f849df4681"
+      sha256 "c0f7f7c26b173acdb9f3399a15140ab728c7a3361eb274400355fb2b4bc1a56b"
   elsif OS.linux?
       url "https://github.com/Byron/termbook/releases/download/#{version}/termbook-#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "ddd3b78051a12f4d95e816a0e479b3d5dbaa3cb4579e16d7f283c78976993feb"
+      sha256 "87bcf3f800b97f9e6a677022ec045f9152c8dcfc5b3246fb665396f097978b7f"
   end
 
   def install
